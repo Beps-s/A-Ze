4dce1470ace65401f7c61acd2f76c614a676a825
Joosep Elias Tõru <75365872+joosep-proggers@users.noreply.github.com>
2023-01-12 13:12:54 +0200

    HOT-27 Sisselogimine ja registreerimine

---
+++ controllers/user.js
@@ -0,0 +1,48 @@
+import { insertUser, getUser } from "../models/userModel.js";
+
+export const createUser = (req, res) => {
+    const data = req.body;
+    insertUser(data, (err, results) => {
+        if (err){
+            res.send(err);
+        }else{
+            res.json(results);
+        }
+    });
+}
+
+// poolik login
+
+export const loginUser = (req, res) => {
+    const data = req.body
+    getUser(data.Email, (err, results) => {
+        if (err){
+            res.send(err);
+        }else{
+            if (data.parool == results[0].parool && data.Email == results[0].Email){
+                let sessionID = Math.round(Math.random() * 100000000)
+                sessions.push({user: data.Email, sessionID: sessionID})
+                res.json({sessionID: sessionID});
+            } else {
+                res.json({ error: "Vale e-mail või parool"})
+            }
+        }
+    });
+}
+
+export const logoutUser = (req, res) => {
+    const data = req.body
+    sessions.forEach((element) => {
+        if (
+          element.user == data.Email ||
+          element.id == data.sessionId
+        ) {
+          sessions.splice(element);
+          return res.status(201).send({ success: true });
+        } else {
+          return res.status(401).send({ error: "Invalid sessionId or username" });
+        }
+      });
+}
+
+export const sessions = [];

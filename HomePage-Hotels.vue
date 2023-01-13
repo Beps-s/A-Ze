fb48655103ae10b76004ab50c38f3d375a116db3
katpoola <65228755+katpoola@users.noreply.github.com>
2023-01-13 13:52:08 +0200

    MyBookings page design added

---
+++ HomePage-Hotels.vue
@@ -0,0 +1,52 @@
+<script>
+import { RouterLink } from 'vue-router';
+
+export default {
+    props: [
+        "nimi",
+        "pilt",
+        "hind",
+        "hotel_id",
+        "aadress"
+    ],
+    components: { RouterLink }
+}
+</script>
+
+
+<template>
+    <div class="col" style="padding-top: 12px; padding-bottom: 12px;">
+        <div class="card home-card h-100">
+            <RouterLink v-bind:to="'/hotel/' + hotel_id" class="card-link">
+                <img v-bind:src="pilt" class="card-img-top">
+                <div class="card-body d-flex justify-content-between align-items-center p-2">
+                    <div>
+                        <h5 id="hotel-name" class="card-title m-0">{{ nimi }}</h5>
+                        <p class="m-0">{{ aadress }}</p>
+                    </div>
+                    <p id="hotel-cost" class="m-0">Alates {{ hind }}€</p>
+                </div>
+            </RouterLink>
+        </div>
+    </div>
+</template>
+
+<style>
+.home-card {
+    background-color: #f4f3ef !important;
+    border: none !important;
+}
+
+a:link {
+    text-decoration: none;
+    color: #000000;
+}
+
+.card-link:visited {
+    color: gray;
+}
+
+a:hover {
+    color: #6f4af0 !important;
+}
+</style>
---
+++ HomePage.vue
@@ -0,0 +1,117 @@
+<script>
+import Hotel from "../components/HomePage-Hotels.vue"
+
+export default {
+    data() {
+        return {
+            hotels: []
+        }
+    },
+    components: {
+        Hotel
+    },
+    methods: {
+        async getAllHotels() {
+            const response = await fetch("http://192.168.16.94:5000/hotels");
+            const finalResponse = await response.json();
+            finalResponse.forEach(element => {
+                this.hotels.push(element)
+            });
+        }
+    },
+    mounted() {
+        setTimeout(() => this.getAllHotels(), 700)
+    }
+}
+</script>
+
+
+<template>
+    <div class="row row-cols-4 my-5">
+        <svg v-if="!hotels.length" class="spinner col-2 mx-auto" viewBox="0 0 50 50" style="margin-top: 33vh;">
+            <circle class="path" cx="25" cy="25" r="20" fill="none" stroke-width="5"></circle>
+        </svg>
+        <div v-else v-for="hotel in hotels">
+            <Hotel :hotel_id="hotel.Hotelli_ID" :nimi="hotel.Nimi" :aadress="hotel.Aadress" :hind="hotel.Hind"
+                :pilt="hotel.pilt" />
+        </div>
+    </div>
+</template>
+
+<style>
+* {
+    font-family: "Roboto", sans-serif;
+}
+
+body {
+    background-color: #f4f3ef !important;
+}
+
+.btn-primary {
+    background: -webkit-linear-gradient(right, #6F4AF0, #c94ef2) !important;
+}
+
+.btn-primary:hover {
+    background: #6f4af0 !important;
+}
+
+.header-link {
+    text-decoration: none;
+    color: #000000;
+    font-weight: 600;
+}
+
+a {
+    cursor: pointer;
+}
+
+.btn-primary {
+    background: -webkit-linear-gradient(right, #6f4af0, #c94ef2) !important;
+    border: -webkit-linear-gradient(right, #6f4af0, #c94ef2) !important;
+}
+
+.btn-primary:hover {
+    background: #6f4af0 !important;
+    border: none !important;
+}
+
+.form-control {
+    box-shadow: none !important;
+}
+
+.spinner {
+    animation: rotate 2s linear infinite;
+    z-index: 2;
+    width: 50px;
+    height: 50px;
+}
+
+.spinner .path {
+    stroke: #947af1;
+    stroke-linecap: round;
+    animation: dash 1.5s ease-in-out infinite;
+}
+
+@keyframes rotate {
+    100% {
+        transform: rotate(360deg);
+    }
+}
+
+@keyframes dash {
+    0% {
+        stroke-dasharray: 1, 150;
+        stroke-dashoffset: 0;
+    }
+
+    50% {
+        stroke-dasharray: 90, 150;
+        stroke-dashoffset: -35;
+    }
+
+    100% {
+        stroke-dasharray: 90, 150;
+        stroke-dashoffset: -124;
+    }
+}
+</style>
